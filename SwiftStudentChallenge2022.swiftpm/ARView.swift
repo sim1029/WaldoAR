cf751b0af27de9fb0ffe409c596f89a80a2dbc31
sim1029 <sps67@pitt.edu>
2022-04-20 08:42:43 -0400

    Render first 2D image

--- SwiftStudentChallenge2022.swiftpm/ARView.swift
+++ SwiftStudentChallenge2022.swiftpm/ARView.swift
@@ -23,7 +23,7 @@ struct ARViewIndicator: UIViewControllerRepresentable {
 import Foundation
 import ARKit
 
-class ARView: UIViewController, ARSKViewDelegate {
+class ARView: UIViewController, ARSKViewDelegate, SKSceneDelegate  {
   
   var arView: ARSKView {
     return self.view as! ARSKView
@@ -33,16 +33,48 @@ class ARView: UIViewController, ARSKViewDelegate {
     self.view = ARSKView(frame: .zero)
   }
   
+  var isWorldSetup = false
+  
+  private func setUpWorld() {
+    guard let currentFrame = arView.session.currentFrame
+    else { return }
+    
+    // Create a transform with a translation of 0.2 meters in front of the camera.
+    var translation = matrix_identity_float4x4
+    translation.columns.3.z = -0.2
+    let transform = simd_mul(currentFrame.camera.transform, translation)
+    
+    // Add a new anchor to the session.
+    let anchor = ARAnchor(transform: transform)
+    arView.session.add(anchor: anchor)
+    
+    isWorldSetup = true
+  }
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     arView.delegate = self
-    let scene = SKScene()
+    let scene = SKScene(size: view.bounds.size)
+    scene.scaleMode = .resizeFill
+    scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    scene.delegate = self
     arView.presentScene(scene)
+    arView.showsFPS = true
+    arView.showsNodeCount = true
   }
+  
+  func update(_ currentTime: TimeInterval, for scene: SKScene) {
+    if !isWorldSetup {
+      setUpWorld()
+    }
+  }
+  
   
   // MARK: - Functions for standard AR view handling
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
+    
   }
   
   override func viewDidLayoutSubviews() {
@@ -71,5 +103,9 @@ class ARView: UIViewController, ARSKViewDelegate {
   
   func session(_ session: ARSession, cameraDidChangeTrackingState
                camera: ARCamera) {}
+  
+  func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+    return SKLabelNode(text: "👾")
+  }
   
 }
